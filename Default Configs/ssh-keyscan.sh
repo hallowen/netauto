9d06753f9ed08e3484fe1ebf6ed87bd181b117cc
sandeepsuresh2006 <sandeepsuresh2006@gmail.com>
2022-04-10 15:27:18 +0000

    Initial Commit

---
+++ Default Configs/ssh-keyscan.sh
@@ -0,0 +1,7 @@
+ssh-keyscan 192.168.0.31 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.0.32 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.0.33 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.0.34 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.0.35 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.0.36 >> ~/.ssh/known_hosts
+ssh-keyscan 192.168.0.37 >> ~/.ssh/known_hosts
